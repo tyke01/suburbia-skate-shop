dac1bb900b3f9d143ac49c6dc9ca4c14ea252cca
victor Macharia <Techvictor653@gmail.com>
2025-01-18 12:01:56 +0300

    feat: added the assets

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,7 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { JSX } from "react";
 
 /**
  * Props for `Hero`.
@@ -9,13 +11,20 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 /**
  * Component for "Hero" Slices.
  */
+
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+  
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for hero (variation: {slice.variation}) Slices
+      <PrismicRichText field={slice.primary.heading} />
+
+      <PrismicRichText field={slice.primary.body} />
+
+      <PrismicNextLink field={slice.primary.button} />
     </section>
   );
 };
