241c040ac59f385459a6926b4ffb109eea9dd1b1
victor Macharia <Techvictor653@gmail.com>
2025-01-19 13:52:35 +0300

    feat: added navigation custom ty[e

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
+
 import { ButtonLink } from "./button-link";
 import { Logo } from "./logo";
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
 
-export const Header = () => {
+export const Header = async () => {
+
+  const client = createClient();
+
+  const settings = await client.getSingle("settings");
+
+
   return (
     <header className="header absolute left-0 right-0 top-0 z-50 ~h-32/48 ~px-4/6  ~py-4/6 md:h-32">
       <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr]">
@@ -15,7 +24,11 @@ export const Header = () => {
           className="col-span-full row-start-1 md:col-span-1 md:row-start-1 md:col-start-2"
         >
           <ul className="flex flex-wrap items-center justify-center gap-8">
-            <li>Boards</li>
+            {settings.data.navigation.map((item) => (
+            <li key={item.link.text}>
+              <PrismicNextLink field={item.link} className="~text-lg/xl" />
+            </li>
+            ))}
           </ul>
         </nav>
 
