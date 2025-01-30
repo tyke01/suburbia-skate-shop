b9e03d7ad8ffba894cf430da34a1fc6fd07f5a97
victor Macharia <Techvictor653@gmail.com>
2025-01-30 12:21:30 +0300

    feat: added matter js physics on the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,35 +1,51 @@
 import { createClient } from "@/prismicio";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next"
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { Logo } from "@/components/logo";
 import { Bounded } from "./bounded";
+import { FooterPhysics } from "./footer-physics";
+import { asImageSrc } from "@prismicio/client";
 
 export const Footer = async () => {
+  const client = createClient();
 
-    const client = createClient();
+  const settings = await client.getSingle("settings");
 
-    const settings = await client.getSingle("settings");
+  const boardTextureUrls = settings.data.footer_skateboards
+    .map((item) => asImageSrc(item.skateboard, { h: 600 }))
+    .filter((url): url is string => Boolean(url));
 
   return (
     <footer className="bg-texture bg-zinc-900 text-white overflow-hidden">
-        <div className="relative  h-[75vh] ~p-10/16 md:aspect-auto">
-        <PrismicNextImage field={settings.data.footer_image} alt="" fill className="object-cover" width={1200}/>
+      <div className="relative  h-[75vh] ~p-10/16 md:aspect-auto">
+        <PrismicNextImage
+          field={settings.data.footer_image}
+          alt=""
+          fill
+          className="object-cover"
+          width={1200}
+        />
 
-        <Logo className="pointer-events-none relative h-20 mix-blend-exclusion md:h-28"/>
+        <FooterPhysics
+          boardTextureURLs={boardTextureUrls}
+          className="absolute inset-0 overflow-hidden"
+        />
+
+        <Logo className="pointer-events-none relative h-20 mix-blend-exclusion md:h-28" />
 
         {/* Board physics */}
-        </div>
+      </div>
 
-        <Bounded as={"nav"}>
-            <ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
-                {settings.data.navigation.map((item) => (
-                    <li key={item.link.text} className="hover:underline">
-                        <PrismicNextLink field={item.link}  />
-                    </li>
-                ))}
-            </ul>
-        </Bounded>
+      <Bounded as={"nav"}>
+        <ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
+          {settings.data.navigation.map((item) => (
+            <li key={item.link.text} className="hover:underline">
+              <PrismicNextLink field={item.link} />
+            </li>
+          ))}
+        </ul>
+      </Bounded>
 
-        {/* list of links */}
+      {/* list of links */}
     </footer>
-  )
-}
+  );
+};
