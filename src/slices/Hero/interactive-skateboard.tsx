a41e9f73b60f36744d3cc3fda81e36a7f7e462e1
victor Macharia <Techvictor653@gmail.com>
2025-02-01 15:49:26 +0300

    update: Finished 3 sick tricks

--- src/slices/Hero/interactive-skateboard.tsx
+++ src/slices/Hero/interactive-skateboard.tsx
@@ -46,12 +46,16 @@ function Scene({
   boltColor,
 }: Props) {
   const containerRef = useRef<THREE.Group>(null);
+  const originRef = useRef<THREE.Group>(null);
 
   function onClick(event: ThreeEvent<MouseEvent>) {
     event.stopPropagation();
 
     const board = containerRef.current;
-    if (!board) return;
+
+    const origin = originRef.current;
+
+    if (!board || !origin) return;
 
     const { name } = event.object;
 
@@ -59,8 +63,12 @@ function Scene({
       ollie(board);
     } else if (name === "middle") {
       kickflip(board);
+    } else if (name === "front") {
+      frontside360(board, origin);
     }
   }
+
+  // ? --------------------------------------------------------------- //
 
   function ollie(board: THREE.Group) {
     jumpBoard(board);
@@ -83,6 +91,7 @@ function Scene({
         ease: "none",
       });
   }
+  // ? --------------------------------------------------------------- //
 
   function kickflip(board: THREE.Group) {
     jumpBoard(board);
@@ -115,6 +124,39 @@ function Scene({
       });
   }
   // ? --------------------------------------------------------------- //
+
+  function frontside360(board: THREE.Group, origin: THREE.Group) {
+    jumpBoard(board);
+
+    gsap
+      .timeline()
+      .to(board.rotation, {
+        x: -0.6,
+        duration: 0.26,
+        ease: "none",
+      })
+      .to(board.rotation, {
+        x: 0.4,
+        duration: 0.82,
+        ease: "power2.in",
+      })
+      .to(
+        origin.rotation,
+        {
+          y: `+=${Math.PI * 2}`,
+          duration: 0.77,
+          ease: "none",
+        },
+        0.3
+      )
+      .to(board.rotation, {
+        x: 0,
+        duration: 0.14,
+        ease: "none",
+      });
+  }
+  // ? --------------------------------------------------------------- //
+
   function jumpBoard(board: THREE.Group) {
     gsap
       .timeline()
@@ -135,32 +177,34 @@ function Scene({
     <group>
       <OrbitControls />
       <Environment files={"/hdr/warehouse-256.hdr"} />
-      <group ref={containerRef} position={[-0.25, 0, -0.635]}>
-        <group position={[0, -0.086, 0.635]}>
-          <Skateboard
-            wheelTextureURLs={[wheelTextureURL]}
-            wheelTextureURL={wheelTextureURL}
-            deckTextureURLs={[deckTextureURL]}
-            deckTextureURL={deckTextureURL}
-            truckColor={truckColor}
-            boltColor={boltColor}
-            constantWheelSpin
-          />
+      <group ref={originRef}>
+        <group ref={containerRef} position={[-0.25, 0, -0.635]}>
+          <group position={[0, -0.086, 0.635]}>
+            <Skateboard
+              wheelTextureURLs={[wheelTextureURL]}
+              wheelTextureURL={wheelTextureURL}
+              deckTextureURLs={[deckTextureURL]}
+              deckTextureURL={deckTextureURL}
+              truckColor={truckColor}
+              boltColor={boltColor}
+              constantWheelSpin
+            />
 
-          <mesh position={[0, 0.27, 0.9]} name="front" onClick={onClick}>
-            <boxGeometry args={[0.6, 0.2, 0.58]} />
-            <meshStandardMaterial visible={false} />
-          </mesh>
+            <mesh position={[0, 0.27, 0.9]} name="front" onClick={onClick}>
+              <boxGeometry args={[0.6, 0.2, 0.58]} />
+              <meshStandardMaterial visible={false} />
+            </mesh>
 
-          <mesh position={[0, 0.27, 0]} name="middle" onClick={onClick}>
-            <boxGeometry args={[0.6, 0.1, 1.2]} />
-            <meshStandardMaterial visible={false} />
-          </mesh>
+            <mesh position={[0, 0.27, 0]} name="middle" onClick={onClick}>
+              <boxGeometry args={[0.6, 0.1, 1.2]} />
+              <meshStandardMaterial visible={false} />
+            </mesh>
 
-          <mesh position={[0, 0.27, -0.9]} name="back" onClick={onClick}>
-            <boxGeometry args={[0.6, 0.2, 0.58]} />
-            <meshStandardMaterial visible={false} />
-          </mesh>
+            <mesh position={[0, 0.27, -0.9]} name="back" onClick={onClick}>
+              <boxGeometry args={[0.6, 0.2, 0.58]} />
+              <meshStandardMaterial visible={false} />
+            </mesh>
+          </group>
         </group>
       </group>
       <ContactShadows opacity={0.6} position={[0, -0.08, 0]} />
