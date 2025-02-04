9d287db3fc5efc684a06e12fea7be03b25113f6a
victor Macharia <Techvictor653@gmail.com>
2025-02-04 17:05:38 +0300

    fix: typescript errors

--- src/components/bounded.tsx
+++ src/components/bounded.tsx
@@ -15,9 +15,9 @@ export function Bounded({
   ...restProps
 }: BoundedProps) {
   return (
-    // @ts-expect-error
+    // @ts-expect-error don't know how to fix
     <Comp
-      // @ts-expect-error
+      // @ts-expect-error don't know how to fix
       className={clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
         className
